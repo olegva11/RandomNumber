aa3ab9f0b6930c551dc69129be47413c068ae4c6
Lenovo <olegva11@gmail.com>
2024-02-07 23:37:38 +0200

    Add viewModel for working with Api from UI

--- app/src/main/java/com/example/randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/randomnumber/MainActivity.kt
@@ -3,31 +3,20 @@ package com.example.randomnumber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Info
-import androidx.compose.material.icons.sharp.Search
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.randomnumber.ui.MainViewModel
 import com.example.randomnumber.ui.theme.RandomNumberTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InterestingNumbersApp()
+                    InterestingNumbersApp(viewModel)
                 }
             }
         }
@@ -45,20 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun InterestingNumbersApp()
+fun InterestingNumbersApp(viewModel: MainViewModel)
 {
     Column {
-        SearchBox()
+        SearchBox(viewModel = viewModel)
         HistoryBox()
     }
-
 }
-
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     RandomNumberTheme {
-        SearchBox()
+
     }
 }
--- app/src/main/java/com/example/randomnumber/SearchBox.kt
+++ app/src/main/java/com/example/randomnumber/SearchBox.kt
@@ -1,9 +1,11 @@
 package com.example.randomnumber
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.sharp.Info
 import androidx.compose.material.icons.sharp.Search
@@ -15,28 +17,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.randomnumber.ui.MainViewModel
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun SearchBox(modifier: Modifier = Modifier) {
-    val inputNumber by remember {
+fun SearchBox(modifier: Modifier = Modifier, viewModel: MainViewModel) {
+    val inputNumber by rememberSaveable {
         mutableIntStateOf(0)
     }
+
+    val mutableNumber = remember { mutableIntStateOf(inputNumber) }
 
     Column(modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
             modifier = Modifier,
             text = "Input number and get info:"
         )
-        TextField(value = inputNumber.toString(), onValueChange = {})
+        TextField(value = mutableNumber.intValue.toString(), onValueChange = {
+            if (it.isNotEmpty()) {
+                mutableNumber.intValue = it.toInt()
+            } else {
+                mutableNumber.intValue = 0
+            }
+        }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number))
         Button(modifier = Modifier, onClick = {
+            viewModel.getInfoByNumber(mutableNumber.intValue)
         }) {
             Row {
                 Icon(
                     Icons.Sharp.Search,
-                    contentDescription = "lolka",
+                    contentDescription = "",
                     modifier = Modifier
                         .padding(1.dp)
                 )
@@ -45,11 +60,12 @@ fun SearchBox(modifier: Modifier = Modifier) {
         }
 
         Button(modifier = Modifier, onClick = {
+            viewModel.getInfoByRandomNumber()
         }) {
             Row {
                 Icon(
                     Icons.Sharp.Info,
-                    contentDescription = "lolka",
+                    contentDescription = "",
                     modifier = Modifier
                         .padding(1.dp)
                 )
